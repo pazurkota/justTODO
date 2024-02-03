d2d742e5815caa7d26066913729cb0bdd76ba5f7
Piotr Jelonek <piotrek.jelonek@icloud.com>
2024-02-03 13:23:43 +0100

    Added MainPage.xaml and ToDoPage.xaml in View folder

---
+++ justtodo/View/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace justtodo.View;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ justtodo/View/ToDoPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace justtodo.View;
+
+public partial class ToDoPage : ContentPage
+{
+	public ToDoPage()
+	{
+		InitializeComponent();
+	}
+}
